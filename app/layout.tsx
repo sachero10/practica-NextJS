739b0b9faf68cd04a15003f2a94336c907887833
Alan <sachero10@gmail.com>
2024-08-14 19:30:14 -0300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "./ui/global.css";
+import { montserrat } from "./ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="py-10 flex justify-center items-center">
+          Hecho por Alan 💪🏼
+        </footer>
+      </body>
     </html>
   );
 }
